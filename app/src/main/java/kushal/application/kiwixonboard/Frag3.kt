1ae566af287329bf9b55d4642d99292a82f12474
KUSHAL <kushalgera1212@gamil.com>
2020-03-16 22:23:48 +0530

    changes

--- app/src/main/java/kushal/application/kiwixonboard/Frag3.kt
+++ app/src/main/java/kushal/application/kiwixonboard/Frag3.kt
@@ -1,10 +1,11 @@
 package kushal.application.kiwixonboard
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_frag3.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +18,9 @@ class Frag3 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val vieww = inflater.inflate(R.layout.fragment_frag3, container, false)
-        
+
+        vieww.frame.animate().translationYBy(-40f).alpha(1f).duration = 1600
+
         return vieww
     }
 
